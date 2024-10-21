be7e249fb2f159251a0940d0a8677b833ca8186f
sina <20732540+SinaKhalili@users.noreply.github.com>
2024-10-21 16:03:47 +0800

    update backend api to have LRU cache

--- backend/api/asset_liability.py
+++ backend/api/asset_liability.py
@@ -9,21 +9,28 @@ from fastapi import APIRouter
 router = APIRouter()
 
 
-@router.get("/matrix/{mode}/{perp_market_index}")
 async def get_asset_liability_matrix(
-    request: BackendRequest, mode: int, perp_market_index: int
-):
-    backend_state: BackendState = request.state.backend_state
-    vat: Vat = backend_state.vat
-
-    with waiting_for("Getting asset liability matrix"):
-        res, df = await get_matrix(vat, mode, perp_market_index)
-
-    with waiting_for("Converting to dict"):
-        res_dict = res.to_dict()
-        df_dict = df.to_dict()
+    snapshot_path: str, vat: Vat, mode: int, perp_market_index: int
+) -> dict:
+    print("==> Getting asset liability matrix...")
+    res, df = await get_matrix(vat, mode, perp_market_index)
+    res_dict = res.to_dict()
+    df_dict = df.to_dict()
+    print("==> Asset liability matrix fetched")
 
     return {
         "res": res_dict,
         "df": df_dict,
     }
+
+
+@router.get("/matrix/{mode}/{perp_market_index}")
+async def get_asset_liability_matrix(
+    request: BackendRequest, mode: int, perp_market_index: int
+):
+    return await get_asset_liability_matrix(
+        request.state.current_pickle_path,
+        request.state.backend_state.vat,
+        mode,
+        perp_market_index,
+    )
--- backend/api/price_shock.py
+++ backend/api/price_shock.py
@@ -1,3 +1,4 @@
+import functools
 from typing import Optional
 
 from backend.state import BackendRequest
@@ -11,18 +12,16 @@ from fastapi import APIRouter
 router = APIRouter()
 
 
-@router.get("/usermap")
-async def get_price_shock(
-    request: BackendRequest,
+@functools.lru_cache()
+async def _get_price_shock(
+    snapshot_path: str,
+    vat: Vat,
+    drift_client: DriftClient,
     oracle_distortion: float = 0.1,
     asset_group: Optional[str] = None,
     n_scenarios: int = 5,
-):
-    backend_state: BackendState = request.state.backend_state
-    vat: Vat = backend_state.vat
-    drift_client: DriftClient = backend_state.dc
-
-    result = get_user_leverages_for_price_shock(
+) -> dict:
+    return get_user_leverages_for_price_shock(
         drift_client,
         vat.users,
         oracle_distortion,
@@ -30,4 +29,19 @@ async def get_price_shock(
         n_scenarios,
     )
 
-    return result
+
+@router.get("/usermap")
+async def get_price_shock(
+    request: BackendRequest,
+    oracle_distortion: float = 0.1,
+    asset_group: Optional[str] = None,
+    n_scenarios: int = 5,
+):
+    return await _get_price_shock(
+        request.state.backend_state.current_pickle_path,
+        request.state.backend_state.vat,
+        request.state.backend_state.dc,
+        oracle_distortion,
+        asset_group,
+        n_scenarios,
+    )
